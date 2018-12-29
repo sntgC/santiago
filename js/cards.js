a1447141e8a941e70106da03b118116f9a74fad2
Santiago <sntgcortes23@gmail.com>
2018-12-28 21:24:56 -0600

    Data changes

--- js/cards.js
+++ js/cards.js
@@ -1,34 +1,70 @@
 var projects = [{
-    id:2,
-    date:"Summer 2018",
-    name:"Buzz Buzz",
-    image:"buzzbuzz.jpg",
-    descShort:"Websocket-based buzzer app.",
-    descFull:"Websocket-based <b>buzzer</b> app.",
-    tags:[{
-        img:"gopher.png",
-        title:"Golang"
+        id: 2,
+        date: "Summer 2018",
+        name: "Buzz Buzz",
+        image: "buzzbuzz.jpg",
+        descShort: "Websocket-based buzzer app.",
+        descFull: "Websocket-based <b>buzzer</b> app.",
+        tags: [{
+            img: "gopher.png",
+            title: "Golang"
     }]
-}];
+},
+    {
+        name: "Project Aether",
+        date:"May 2017",
+        image: "earth.JPG",
+        descShort: "Weather baloon tracking website.",
+        descFull:"Weather baloon website complete with real-time tracking and data collection. Now an archive website storing the collected data, images, and videos.",
+        link: "https://cdogg99.github.io/aether-project-site/",
+        tags: [{
+            img: "javascript.png",
+            title: "JavaScript"
+    }]
+    },
+    {
+        name: "UT<sup>4</sup>",
+        date:"Summer 2016",
+        image: "utfour.png",
+        descShort: "Ultimate Tic-Tac-Toe tournament creator.",
+        link: "https://github.com/sntgC/utfour",
+        descFull:"",
+        tags: [{
+            img: "javascript.png",
+            title: "JavaScript"
+    }]
+    },
+    {
+        name: "Bunn's: Burgers and Fun",
+        date:"Spring 2017",
+        image: "bunns.png",
+        descShort: "Mock website for a design competition",
+        descFull:"",
+        link: "https://sntgc.github.io/Bunn-s/index",
+        tags: [{
+            img: "javascript.png",
+            title: "JavaScript"
+    }]
+    }];
 
 var education = [{
-    id:1,
-    date:"2018-2022",
-    name:"University of Texas - Austin",
-    image:"utaustin.jpg",
-    descShort:"UT 22 🤘",
-    descFull:"",
-    tags:[]
+    id: 1,
+    date: "2018-2022",
+    name: "University of Texas - Austin",
+    image: "utaustin.jpg",
+    descShort: "UT 22 🤘",
+    descFull: "I am currently studying Computer Science at the University of Texas at Austin under the Turing Scholars program.",
+    tags: []
 }];
 
 var experience = [{
-    id:3,
-    date:"Summer 2019",
-    name:"Dropbox SWE Intern",
-    image:"dropbox.jpg",
-    descShort:"San Francisco office.",
-    descFull:"",
-    tags:[]
+    id: 3,
+    date: "Summer 2019",
+    name: "Dropbox SWE Intern",
+    image: "dropbox.jpg",
+    descShort: "Future Dropbox intern.",
+    descFull: "I will be working for Dropbox for 12 weekds during the summer at their San Francisco location as a Software Engineering intern.",
+    tags: []
 }]
 
-var data=[projects,education,experience];
+var data = [projects, education, experience];
