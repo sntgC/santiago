fa80b2fe9d4ec486f2f1d04e5d6246297f271a46
Santiago <sntgC>
2019-01-02 20:19:08 -0600

    Resizing using js

--- js/slide.js
+++ js/slide.js
@@ -6,7 +6,31 @@ $(document).ready(function(){
         children=$(e).children();
         for(i=0;i<children.length;i++){
             $(children[i]).css("left","-"+(25.125*(i))+"%");
-            console.log($(children[i]).css("left"));
         }
     });
+    fitCards();
+    $(window).resize(fitCards)
 });
+
+function fitCards(){
+    var width=$("#content-box").width();
+    var cardWidth=$(document.getElementsByClassName("card")[0]).width();
+    var numCards=Math.floor(width/cardWidth);
+    var margin=(width%cardWidth)/(numCards+1);
+    if(margin<15){
+        margin=(width%cardWidth+cardWidth)/(numCards);
+        numCards--;
+    }
+    if(width<635){
+        margin=($(window).width()-151-cardWidth)/2;
+    }
+    var cards=document.getElementsByClassName("card");
+    for(i=0;i<cards.length;i++){
+        $(cards[i]).css("margin","10px "+(margin/2)+"px");
+        if(i%numCards==0)
+            $(cards[i]).css("margin-left",margin+"px");
+        if(i%numCards==numCards-1)
+            $(cards[i]).css("margin-right",margin+"px");
+    }
+    console.log(width+" "+cardWidth);
+}
